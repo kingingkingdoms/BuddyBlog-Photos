e529ce6bbe24421ad5b470140dd44fa48ad11cd6
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-06-13 17:30:51 -0400

    Update posts.php

--- template/buddyblog/posts.php
+++ template/buddyblog/posts.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<?php if ( buddyblogmusic_user_has_posted() ): ?>
+<?php if ( buddyblogarticles_user_has_posted() ): ?>
 <?php
     //let us build the post query
     if ( bp_is_my_profile() || is_super_admin() ) {
@@ -18,7 +18,7 @@
     
 	$query_args = array(
 		'author'        => bp_displayed_user_id(),
-		'post_type'     => buddyblogmusic_get_posttype(),
+		'post_type'     => buddyblogarticles_get_posttype(),
 		'post_status'   => $status,
 		'paged'         => intval( $paged )
     );
@@ -34,52 +34,52 @@
 
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                <div class="author-box">
+                <div class="buddyblog-article-author-box">
                     <?php echo get_avatar( get_the_author_meta( 'user_email' ), '50' ); ?>
-                    <p><?php printf( _x( 'by %s', 'Post written by...', 'buddyblogmusic' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
+                    <p><?php printf( _x( 'by %s', 'Post written by...', 'buddyblogarticles' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
 
                     <?php if ( is_sticky() ) : ?>
-                        <span class="activity sticky-post"><?php _ex( 'Featured', 'Sticky post', 'buddyblogmusic' ); ?></span>
+                        <span class="activity sticky-post"><?php _ex( 'Featured', 'Sticky post', 'buddyblogarticles' ); ?></span>
                     <?php endif; ?>
                 </div>
 
-                <div class="post-content">
+                <div class="buddyblog-article-content">
                     
                     <?php if ( function_exists( 'has_post_thumbnail' ) && has_post_thumbnail( get_the_ID() ) ):?>
                         
-                        <div class="post-featured-image">
+                        <div class="buddyblog-article-featured-image">
                             <?php  the_post_thumbnail();?>
                         </div>
 
                     <?php endif;?>
 
-                    <h2 class="posttitle"> <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddyblogmusic' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h2>
+                    <h2 class="buddyblog-article-title"> <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddyblogarticles' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h2>
 
-                    <p class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddyblogmusic' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
+                    <p class="buddyblog-article-date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddyblogarticles' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
 
-                    <div class="entry">
+                    <div class="buddyblog-article-entry">
 
-                        <?php the_content( __( 'Read the rest of this entry &rarr;', 'buddyblogmusic' ) ); ?>
-                        <?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddyblogmusic' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
+                        <?php the_content( __( 'Read the rest of this entry &rarr;', 'buddyblogarticles' ) ); ?>
+                        <?php wp_link_pages( array( 'before' => '<div class="buddyblog-article-page-link"><p>' . __( 'Pages: ', 'buddyblogarticles' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
                     </div>
 
-                    <p class="postmetadata"><?php the_tags( '<span class="tags">' . __( 'Tags: ', 'buddyblogmusic' ), ', ', '</span>' ); ?> <span class="comments"><?php comments_popup_link( __( 'No Comments &#187;', 'buddyblogmusic' ), __( '1 Comment &#187;', 'buddyblogmusic' ), __( '% Comments &#187;', 'buddyblogmusic' ) ); ?></span></p>
+                    <p class="buddyblog-article-meta-data"><?php the_tags( '<span class="buddyblog-article-tags">' . __( 'Tags: ', 'buddyblogarticles' ), ', ', '</span>' ); ?> <span class="buddyblog-article-comments"><?php comments_popup_link( __( 'No Comments &#187;', 'buddyblogarticles' ), __( '1 Comment &#187;', 'buddyblogarticles' ), __( '% Comments &#187;', 'buddyblogarticles' ) ); ?></span></p>
 
-                    <div class="post-actions">
-                        <?php echo buddyblogmusic_get_post_publish_unpublish_link( get_the_ID() );?>
-                        <?php echo buddyblogmusic_get_edit_link();?>
-                        <?php echo buddyblogmusic_get_delete_link();?>
+                    <div class="buddyblog-article-actions">
+                        <?php echo buddyblogarticles_get_post_publish_unpublish_link( get_the_ID() );?>
+                        <?php echo buddyblogarticles_get_edit_link();?>
+                        <?php echo buddyblogarticles_get_delete_link();?>
                     </div>     
                 </div>
 
 			</div>
                    
         <?php endwhile;?>
-            <div class="pagination">
-                <?php buddyblogmusic_paginate(); ?>
+            <div class="buddyblog-article-pagination">
+                <?php buddyblogarticles_paginate(); ?>
             </div>
     <?php else: ?>
-            <p><?php _e( 'There are no posts by this user at the moment. Please check back later!', 'buddyblogmusic' );?></p>
+            <p><?php _e( 'There are no posts by this user at the moment. Please check back later!', 'buddyblogarticles' );?></p>
     <?php endif; ?>
 
     <?php 
@@ -87,7 +87,7 @@
        wp_reset_query();
     ?>
 
-<?php elseif ( bp_is_my_profile() && buddyblogmusic_user_can_post( get_current_user_id() ) ): ?>
-    <p> <?php _e( "You haven't posted anything yet.", 'buddyblogmusic' );?> <a href="<?php echo buddyblogmusic_get_new_url();?>"> <?php _e( 'New Post', 'buddyblogmusic' );?></a></p>
+<?php elseif ( bp_is_my_profile() && buddyblogarticles_user_can_post( get_current_user_id() ) ): ?>
+    <p> <?php _e( "You haven't posted anything yet.", 'buddyblogarticles' );?> <a href="<?php echo buddyblogarticles_get_new_url();?>"> <?php _e( 'New Post', 'buddyblogarticles' );?></a></p>
 
 <?php endif; ?>
