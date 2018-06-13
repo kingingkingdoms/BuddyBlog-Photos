9b77e0e4d00df16ef34fbfa094f5e284914d6061
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-06-13 18:07:56 -0400

    Update buddyblogarticles-screens.php

--- core/buddyblogarticles-screens.php
+++ core/buddyblogarticles-screens.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Screen Handler.
  */
-class BuddyBlogArticles_Screens {
+class BuddyBlogPhotos_Screens {
 
 	/**
 	 * Singleton instance.
@@ -50,9 +50,9 @@ class BuddyBlogArticles_Screens {
 	/**
 	 * Handles My Posts screen with the single post/edit post view
 	 */
-	public function my_articles() {
+	public function my_photos() {
 
-		if ( buddyblogarticles_is_single_post() ) {
+		if ( buddyblogphotos_is_single_post() ) {
 			add_action( 'bp_template_content', array( $this, 'get_single_post_data' ) );
 		} else { // list all posts by user.
 			add_action( 'bp_template_content', array( $this, 'get_posts_list_data' ) );
@@ -89,7 +89,7 @@ class BuddyBlogArticles_Screens {
 
 		ob_start();
 
-		buddyblogarticles_load_template( 'single.php' );
+		buddyblogphotos_load_template( 'single.php' );
 
 		$content = ob_get_clean();
 
@@ -103,7 +103,7 @@ class BuddyBlogArticles_Screens {
 
 		ob_start();
 
-		buddyblogarticles_load_template( 'posts.php' );
+		buddyblogphotos_load_template( 'posts.php' );
 
 		$content = ob_get_clean();
 
@@ -117,7 +117,7 @@ class BuddyBlogArticles_Screens {
 
 		ob_start();
 
-		buddyblogarticles_load_template( 'edit.php' );
+		buddyblogphotos_load_template( 'edit.php' );
 
 		$content = ob_get_clean();
 
@@ -127,4 +127,4 @@ class BuddyBlogArticles_Screens {
 }
 
 
-BuddyBlogArticles_Screens::get_instance();
+BuddyBlogPhotos_Screens::get_instance();
