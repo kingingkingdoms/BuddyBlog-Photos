83d60dfcc09c01815ca9f90a5627c78852994252
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-06-13 17:27:58 -0400

    Update buddyblogmusic-hooks.php

--- core/buddyblogmusic-hooks.php
+++ core/buddyblogmusic-hooks.php
@@ -12,23 +12,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Fix to disable/re-enable buddypress comment open/close filter.
  */
-function buddyblogmusic_disable_bp_comment_filter() {
+function buddyblogarticles_disable_bp_comment_filter() {
 
 	if ( has_filter( 'comments_open', 'bp_comments_open' ) ) {
 		remove_filter( 'comments_open', 'bp_comments_open', 10, 2 );
 	}
 }
 
-add_action( 'buddyblogmusic_before_blog_post', 'buddyblogmusic_disable_bp_comment_filter' );
+add_action( 'buddyblogarticles_before_blog_post', 'buddyblogarticles_disable_bp_comment_filter' );
 
 /**
  * Re enable buddypress comments filter.
  */
-function buddyblogmusic_enable_bp_comment_filter() {
+function buddyblogarticles_enable_bp_comment_filter() {
 
 	if ( function_exists( 'bp_comments_open' ) ) {
 		add_filter( 'comments_open', 'bp_comments_open', 10, 2 );
 	}
 }
 
-add_action( 'buddyblogmusic_after_blog_post', 'buddyblogmusic_enable_bp_comment_filter' );
+add_action( 'buddyblogarticles_after_blog_post', 'buddyblogarticles_enable_bp_comment_filter' );
