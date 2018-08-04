de4dd247e95e22d50e5b6ae4b5f2b7e92cc54134
K&L <37118190+millionairesdigest@users.noreply.github.com>
2018-08-04 10:46:28 -0400

    Update buddyblogphotos.php

--- buddyblogphotos.php
+++ buddyblogphotos.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * Plugin Name: BuddyBlog Photos
- * Version: 1.3.2
- * Author: K&L
- * Author URI: https://millionairedigest.com/members/kl/
- * Plugin URI: https://millionairedigest.com/
- * Description: Add a BuddyPress profile nav tab called "Photos" to user's profiles for giving our users the ability to display all of the photos they've taken.
- *
- * @package buddyblog
- */
+/*
+Plugin Name: Millionaire's Digest Profile Photos
+Description: Add a BuddyPress profile nav tab called "Photos" to user's profiles for giving our users the ability to display all of the photos they've taken.
+Version: 1.0.0
+Author: K&L (Founder of the Millionaire's Digest)
+Author URI: https://millionairedigest.com/
+*/
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
